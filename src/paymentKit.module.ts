fb98ec20ccacb9711e98e5cc8a2d9fbf8d862a6f
Zaiidmo <zaiidmoumnii@gmail.com>
2025-11-28 16:15:01 +0100

    WebhookSystem: Inject the webhookController into PaymentKit Module

--- src/paymentKit.module.ts
+++ src/paymentKit.module.ts
@@ -2,6 +2,7 @@ import { PAYMENTKIT_CONFIG } from '@common/constants';
 import { PaymentKitPublicConfig } from '@config/paymentKit.config';
 import { PaymentKitConfigLoader } from '@config/paymentKit.config-loader';
 import { DynamicModule, Module } from '@nestjs/common';
+import { InternalWebhookController } from './nest/controllers/internal-webhook.controller';
 
 @Module({})
 export class PaymentKitModule {
@@ -10,6 +11,7 @@ export class PaymentKitModule {
 
     return {
       module: PaymentKitModule,
+      controllers: [InternalWebhookController],
       providers: [{ provide: PAYMENTKIT_CONFIG, useValue: resolvedConfig }],
       exports: [PAYMENTKIT_CONFIG],
     };
