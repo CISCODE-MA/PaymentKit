c9f057d26a02adacb6ebc662f80e1e86f1e1e1dd
Zaiidmo <zaiidmoumnii@gmail.com>
2025-12-01 10:15:23 +0100

    WebhookSystem: Wiring dispatcher into PaymentKitModule

--- src/paymentKit.module.ts
+++ src/paymentKit.module.ts
@@ -4,11 +4,15 @@ import { PaymentKitPublicConfig } from '@config/paymentKit.config';
 import { PaymentKitConfigLoader } from '@config/paymentKit.config-loader';
 import { InternalWebhookController } from './nest/controllers/internal-webhook.controller';
 import { WebhookGatewayRouter } from '@src/core/services/webhook-gateway-router.service';
+import {
+  InMemoryWebhookEventDispatcher,
+  type WebhookEventDispatcher,
+} from '@src/core/services/webhook-event-dispatcher.service';
 
 @Module({
   controllers: [InternalWebhookController],
-  providers: [WebhookGatewayRouter],
-  exports: [WebhookGatewayRouter],
+  providers: [WebhookGatewayRouter, InMemoryWebhookEventDispatcher],
+  exports: [WebhookGatewayRouter, InMemoryWebhookEventDispatcher],
 })
 export class PaymentKitModule {
   static register(config: PaymentKitPublicConfig): DynamicModule {
@@ -17,8 +21,19 @@ export class PaymentKitModule {
     return {
       module: PaymentKitModule,
       controllers: [InternalWebhookController],
-      providers: [{ provide: PAYMENTKIT_CONFIG, useValue: resolvedConfig }, WebhookGatewayRouter],
-      exports: [PAYMENTKIT_CONFIG, WebhookGatewayRouter],
+      providers: [
+        { provide: PAYMENTKIT_CONFIG, useValue: resolvedConfig },
+        {
+          provide: InMemoryWebhookEventDispatcher,
+          useClass: InMemoryWebhookEventDispatcher,
+        },
+        {
+          provide: WebhookGatewayRouter,
+          useFactory: (dispatcher: WebhookEventDispatcher) => new WebhookGatewayRouter(dispatcher),
+          inject: [InMemoryWebhookEventDispatcher],
+        },
+      ],
+      exports: [PAYMENTKIT_CONFIG, WebhookGatewayRouter, InMemoryWebhookEventDispatcher],
     };
   }
 }
