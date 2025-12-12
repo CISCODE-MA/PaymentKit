8eadee81d03a8c1b91d68e56633a002cd5683816
Zaiidmo <zaiidmoumnii@gmail.com>
2025-12-12 11:57:23 +0100

    Refactor: fixing lint issues

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-// Public API surface for @ciscode/PaymentKit
-export { PaymentKitModule } from './paymentKit.module';
-
-// Nest-facing public service
-export { PaymentsService } from './nest/services/payments.service';
-
-// Types most host apps will need
-export type { PaymentKitPublicConfig } from './config/paymentKit.config';
 export { PAYMENTKIT_CONFIG } from './common/constants';
