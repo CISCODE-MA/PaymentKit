a0ed2bcb09906ed3c7be21290a71e396bea636cd
Zaiidmo <zaiidmoumnii@gmail.com>
2025-12-15 17:58:26 +0100

    chore(ci): Update the build strategies as long as the workflow file, to make the proper lib artifacts;

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { PAYMENTKIT_CONFIG } from './common/constants';
+export { PaymentKitModule } from './paymentKit.module';
+export { PaymentsService } from './nest/services/payments.service';
+export type { PaymentKitPublicConfig } from './config/paymentKit.config';
